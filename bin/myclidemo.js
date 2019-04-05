285d21540551871d4d0698d02921fcd23a933d23
chousann <1769862201@qq.com>
2019-04-06 01:03:07 +0800

    init

---
+++ bin/myclidemo.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+// 参考：
+// https://www.cnblogs.com/wmhuang/p/7052587.html
+// http://www.sohu.com/a/275486462_495695
+// https://www.jianshu.com/p/095c968d406f
+
+console.log('version is 1.0.0');
